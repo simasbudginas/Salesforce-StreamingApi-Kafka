b87b87e7a41eee217affc5adaddb5c35519a1124
Simas Budginas <9088765+simasbudginas@users.noreply.github.com>
2020-12-03 22:23:55 +0200

    SF-570 | Relationships are not supported by SF Push Topics

--- salesforce/pushtopic/BookingStatusUpdates.sql
+++ salesforce/pushtopic/BookingStatusUpdates.sql
@@ -2,9 +2,8 @@
 SELECT Id,
        bookingId__c,
        salesforceBookingStatus__c,
-       Distribution_Site__r.brandApiName__c,
-       LastModifiedBy.Id,
-       LastModifiedBy.email,
+       Distribution_Site_Api_Name__c,
+       LastModifiedById,
        LastModifiedDate
 FROM Booking__c
 WHERE (LastModifiedById != '0052p000009Ka2zAAC' AND LastModifiedById != '0052p000009GcLbAAK')
