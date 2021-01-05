b9388ce4f5408a684b5a50ec644f4802c0176971
Simas Budginas <9088765+simasbudginas@users.noreply.github.com>
2021-01-05 17:24:53 +0200

    SF-1009 | Hot fix: use "Select" option for fields notification

--- salesforce/pushtopic/ForgetCustomer/ForgetCustomer.sql
+++ salesforce/pushtopic/ForgetCustomer/ForgetCustomer.sql
@@ -3,10 +3,9 @@ SELECT Id,
        PersonEmail,
        Brand__pc,
        CRM_Status__pc,
-       ForgottenAt__pc,
        LastModifiedById,
        LastModifiedDate
 FROM Account
 WHERE CRM_Status__pc = 'Forgotten'
   AND PersonEmail != null
-  AND (LastModifiedById != '0052p000009Ka2zAAC' AND LastModifiedById != '0052p000009GcLbAAK')
+  AND ExternalId__c != null
